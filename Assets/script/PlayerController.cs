49036252020a2ce761f4650198a2cfe720b4b09f
ningueIT <ningue.o.rei@gmail.com>
2023-09-06 18:21:01 -0300

    Dash :D

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -5,26 +5,118 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _rig; // Componente Rigidbody2D
+    private float horizontal;
+    private float vertical;
+    
+    [SerializeField] private float _Speed;
+    [SerializeField] private float _DashSpeed;
+    [SerializeField] private float _dashingCooldown;
+    [SerializeField] private float _RunSpeed;
+    
+    private bool canDash = true;
+    private bool isDashing = false;
+    private bool isRunning = false;
 
-    [SerializeField]
-    private int _speed; // Velocidade
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private TrailRenderer _tr;
 
-    private void Start()
-    { 
-        _rig = GetComponent<Rigidbody2D>(); // Anexando o componente Rigidbody2D
+    private void Update()
+    {
+        Move();
+        DirectionDash();
+        
+        if (isDashing)
+        {
+            return;
+        }
+        
+        float currentSpeed = isDashing ? _DashSpeed : _Speed;
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = _RunSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+        
+        _rb.velocity = new Vector2(horizontal * currentSpeed, vertical * currentSpeed);
     }
+
     private void FixedUpdate()
     {
-        Move(); // Movimento
+        if (isDashing)
+        {
+            _rb.velocity = _rb.velocity.normalized * _DashSpeed;
+        }
     }
 
-    private void Move() // Movimento
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (SceneManager.GetActiveScene().buildIndex != 0) // Se a cena for diferente de zero(seletor de skin): 
+        if (collision.CompareTag("DashStopper"))
         {
-            _rig.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed, Input.GetAxis("Vertical") * _speed, 0); // Movimentando o Player
+            StopDash();
         }
+    }
 
+    private void StopDash()
+    {
+        isDashing = false;
+        _rb.velocity = Vector2.zero;
+        _tr.emitting = false;
+    }
+
+    private IEnumerator Dash(Vector2 direction)
+    {
+        canDash = false;
+        isDashing = true;
+        _tr.emitting = true;
+        _rb.velocity = direction * _DashSpeed;
+        yield return new WaitForSeconds(_dashingCooldown);
+        StopDash();
+        yield return new WaitForSeconds(0.2f);
+        canDash = true;
+    }
+
+    private void DirectionDash()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && canDash)
+        {
+            Vector2 dashDirection = Vector2.zero;
+            
+            if (Input.GetKey(KeyCode.W))
+            {
+                dashDirection = Vector2.up;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dashDirection = Vector2.down;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dashDirection = Vector2.right;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                dashDirection = Vector2.left;
+            }
+            
+            if (dashDirection != Vector2.zero)
+            {
+                StartCoroutine(Dash(dashDirection));
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+            _rb.velocity = new Vector3(horizontal * _Speed, vertical * _Speed, 0);
+        }
     }
 }
